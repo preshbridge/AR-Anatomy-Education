ef50e6c6c3759a0d4343dd0cedcf6ca659f1b0e8
preshbridge <b.ogbuagu@alustudent.com>
2026-07-05 19:42:51 +0100

    finishing up Topicscene

--- Assets/Scripts/TopicSceneManager.cs
+++ Assets/Scripts/TopicSceneManager.cs
@@ -1,13 +1,52 @@
 using UnityEngine;
-using TMPro;
 using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class TopicSceneManager : MonoBehaviour
 {
-    [Header("UI")]
-    public TMP_Text titleText;
+    [Header("Muscle Cards")]
+    public Button[] muscleCards;
 
-    public TMP_Text[] muscleTexts;
-
+    [Header("Start Learning Button")]
     public Button startLearningButton;
+
+    private int selectedCard = -1;
+
+    private void Start()
+    {
+        // Disable Start Learning
+        startLearningButton.interactable = false;
+
+        // Add click listeners to every muscle card
+        for (int i = 0; i < muscleCards.Length; i++)
+        {
+            int index = i;
+            muscleCards[i].onClick.AddListener(() => SelectCard(index));
+        }
+    }
+
+    void SelectCard(int index)
+    {
+        selectedCard = index;
+
+        // Enable Start Learning
+        startLearningButton.interactable = true;
+
+        // Highlight selected card
+        for (int i = 0; i < muscleCards.Length; i++)
+        {
+            ColorBlock colors = muscleCards[i].colors;
+
+            if (i == index)
+            {
+                colors.normalColor = new Color(0.75f, 1f, 0.75f); // Light green
+            }
+            else
+            {
+                colors.normalColor = Color.white;
+            }
+
+            muscleCards[i].colors = colors;
+        }
+    }
 }
