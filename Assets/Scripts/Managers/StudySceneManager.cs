679a7687d4b4a267f333f41a75a65dc1b8d0bb4d
preshbridge <b.ogbuagu@alustudent.com>
2026-07-12 12:36:00 +0100

    study,topic works

--- Assets/Scripts/Managers/StudySceneManager.cs
+++ Assets/Scripts/Managers/StudySceneManager.cs
@@ -4,16 +4,19 @@ public class StudySceneManager : MonoBehaviour
 {
     public void OpenShoulder()
     {
+        Debug.Log("Opening Shoulder");
         AppManager.Instance.SelectMuscleGroup("Shoulder");
     }
 
     public void OpenUpperArm()
     {
+        Debug.Log("Opening Upper Arm");
         AppManager.Instance.SelectMuscleGroup("Upper Arm");
     }
 
     public void OpenForearm()
     {
+        Debug.Log("Opening Forearm");
         AppManager.Instance.SelectMuscleGroup("Forearm");
     }
 }
