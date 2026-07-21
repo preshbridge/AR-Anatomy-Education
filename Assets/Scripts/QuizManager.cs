3a821213b3da0cb57eef0ace1bdb86f742eb74a8
preshbridge <b.ogbuagu@alustudent.com>
2026-07-21 23:00:18 +0100

    debug

--- Assets/Scripts/QuizManager.cs
+++ Assets/Scripts/QuizManager.cs
@@ -81,27 +81,40 @@ public class QuizManager : MonoBehaviour
         previousButton.interactable = (currentQuestion > 0);
 
         // Next / Submit Buttons
-        if (currentQuestion == questions.Count - 1)
-        {
-            nextButton.gameObject.SetActive(false);
-            submitButton.gameObject.SetActive(true);
-        }
-        else
-        {
-            nextButton.gameObject.SetActive(true);
-            submitButton.gameObject.SetActive(false);
-        }
-    }
+if (currentQuestion == questions.Count - 1)
+{
+    // LAST QUESTION
+    nextButton.gameObject.SetActive(false);
 
-    void SelectAnswer(int answerIndex)
+    submitButton.gameObject.SetActive(true);
+
+    // Only allow submit after selecting an answer
+    submitButton.interactable = (selectedAnswers[currentQuestion] != -1);
+}
+else
+{
+    // ALL OTHER QUESTIONS
+    nextButton.gameObject.SetActive(true);
+    nextButton.interactable = (selectedAnswers[currentQuestion] != -1);
+
+    submitButton.gameObject.SetActive(false);
+}
+
+   void SelectAnswer(int answerIndex)
+{
+    selectedAnswers[currentQuestion] = answerIndex;
+
+    HighlightSelected(answerIndex);
+
+    if (currentQuestion == questions.Count - 1)
     {
-        selectedAnswers[currentQuestion] = answerIndex;
-
-        HighlightSelected(answerIndex);
-
+        submitButton.interactable = true;
+    }
+    else
+    {
         nextButton.interactable = true;
     }
-
+}
     void HighlightSelected(int answerIndex)
     {
         for (int i = 0; i < answerButtons.Length; i++)
@@ -127,7 +140,7 @@ public class QuizManager : MonoBehaviour
             image.color = Color.white;
         }
     }
-
+    }
     public void NextQuestion()
     {
         if (currentQuestion < questions.Count - 1)
@@ -147,85 +160,85 @@ public class QuizManager : MonoBehaviour
     }
 
     public void SubmitQuiz()
+{
+    // Prevent double tap on Android
+    Debug.Log("SUBMIT BUTTON PRESSED");
+    submitButton.interactable = false;
+
+    int score = 0;
+
+    for (int i = 0; i < questions.Count; i++)
     {
-        int score = 0;
-
-        for (int i = 0; i < questions.Count; i++)
-        {
-            if (selectedAnswers[i] == questions[i].correctAnswer)
-                score++;
-        }
-
-        PlayerPrefs.SetInt("QuizScore", score);
-        PlayerPrefs.SetInt("QuizTotal", questions.Count);
-        PlayerPrefs.SetInt("TotalQuestionsAnswered", questions.Count);
-
-        float average = ((float)score / questions.Count) * 100f;
-        PlayerPrefs.SetFloat("AverageScore", average);
-
-        string muscle = AppManager.Instance.SelectedMuscle;
-
-if (muscle == "Deltoid" ||
-    muscle == "Supraspinatus" ||
-    muscle == "Infraspinatus" ||
-    muscle == "Teres Minor" ||
-    muscle == "Subscapularis")
-{
-    float oldScore = PlayerPrefs.GetFloat("ShoulderProgress", 0);
-
-    if (average > oldScore)
-        PlayerPrefs.SetFloat("ShoulderProgress", average);
-}
-
-else if (muscle == "Biceps Brachii" ||
-         muscle == "Brachialis" ||
-         muscle == "Coracobrachialis" ||
-         muscle == "Triceps Brachii")
-{
-    float oldScore = PlayerPrefs.GetFloat("UpperArmProgress", 0);
-
-    if (average > oldScore)
-        PlayerPrefs.SetFloat("UpperArmProgress", average);
-}
-
-else if (muscle == "Brachioradialis" ||
-         muscle == "Flexor Carpi Radialis" ||
-         muscle == "Palmaris Longus" ||
-         muscle == "Flexor Carpi Ulnaris")
-{
-    float oldScore = PlayerPrefs.GetFloat("ForearmProgress", 0);
-
-    if (average > oldScore)
-        PlayerPrefs.SetFloat("ForearmProgress", average);
-}
-
-PlayerPrefs.Save();
-
-        PlayerPrefs.Save();
-
-ProgressManager.Instance.SaveQuizResult(
-    AppManager.Instance.SelectedMuscle,
-    score,
-    questions.Count
-);
-
-PlayerPrefs.SetInt("QuestionCount", questions.Count);
-
-for (int i = 0; i < questions.Count; i++)
-{
-    PlayerPrefs.SetInt("UserAnswer_" + i, selectedAnswers[i]);
-    PlayerPrefs.SetInt("CorrectAnswer_" + i, questions[i].correctAnswer);
-    PlayerPrefs.SetString("Question_" + i, questions[i].question);
-
-    PlayerPrefs.SetString("Option0_" + i, questions[i].answers[0]);
-    PlayerPrefs.SetString("Option1_" + i, questions[i].answers[1]);
-    PlayerPrefs.SetString("Option2_" + i, questions[i].answers[2]);
-    PlayerPrefs.SetString("Option3_" + i, questions[i].answers[3]);
-}
-
-SceneManager.LoadScene("ResultScene");
+        if (selectedAnswers[i] == questions[i].correctAnswer)
+            score++;
     }
 
+    PlayerPrefs.SetInt("QuizScore", score);
+    PlayerPrefs.SetInt("QuizTotal", questions.Count);
+    PlayerPrefs.SetInt("TotalQuestionsAnswered", questions.Count);
+
+    float average = ((float)score / questions.Count) * 100f;
+    PlayerPrefs.SetFloat("AverageScore", average);
+
+    string muscle = AppManager.Instance.SelectedMuscle;
+
+    if (muscle == "Deltoid" ||
+        muscle == "Supraspinatus" ||
+        muscle == "Infraspinatus" ||
+        muscle == "Teres Minor" ||
+        muscle == "Subscapularis")
+    {
+        float oldScore = PlayerPrefs.GetFloat("ShoulderProgress", 0);
+
+        if (average > oldScore)
+            PlayerPrefs.SetFloat("ShoulderProgress", average);
+    }
+    else if (muscle == "Biceps Brachii" ||
+             muscle == "Brachialis" ||
+             muscle == "Coracobrachialis" ||
+             muscle == "Triceps Brachii")
+    {
+        float oldScore = PlayerPrefs.GetFloat("UpperArmProgress", 0);
+
+        if (average > oldScore)
+            PlayerPrefs.SetFloat("UpperArmProgress", average);
+    }
+    else if (muscle == "Brachioradialis" ||
+             muscle == "Flexor Carpi Radialis" ||
+             muscle == "Palmaris Longus" ||
+             muscle == "Flexor Carpi Ulnaris")
+    {
+        float oldScore = PlayerPrefs.GetFloat("ForearmProgress", 0);
+
+        if (average > oldScore)
+            PlayerPrefs.SetFloat("ForearmProgress", average);
+    }
+
+    ProgressManager.Instance.SaveQuizResult(
+        AppManager.Instance.SelectedMuscle,
+        score,
+        questions.Count
+    );
+
+    PlayerPrefs.SetInt("QuestionCount", questions.Count);
+
+    for (int i = 0; i < questions.Count; i++)
+    {
+        PlayerPrefs.SetInt("UserAnswer_" + i, selectedAnswers[i]);
+        PlayerPrefs.SetInt("CorrectAnswer_" + i, questions[i].correctAnswer);
+
+        PlayerPrefs.SetString("Question_" + i, questions[i].question);
+
+        PlayerPrefs.SetString("Option0_" + i, questions[i].answers[0]);
+        PlayerPrefs.SetString("Option1_" + i, questions[i].answers[1]);
+        PlayerPrefs.SetString("Option2_" + i, questions[i].answers[2]);
+        PlayerPrefs.SetString("Option3_" + i, questions[i].answers[3]);
+    }
+
+    PlayerPrefs.Save();
+
+    SceneManager.LoadScene("ResultScene");
+}
     public void GoBack()
     {
         SceneManager.LoadScene("ARScene");
